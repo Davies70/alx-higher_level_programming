0bb3b1bae5f4296777a42929a7beef21f3727327
Ajayi <ajayidavies7@gmail.com>
2022-06-29 07:44:09 -0700

    justified the format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for n in range (0, 10):
-        if i != 9 and n != 9:
-            print("{}{}, ".format(i,n), end='')
+for i in range(0, 99):
+    print("{:0>2d}, ".format(i), end='')
 print(99)
-
-        
-
-
-
