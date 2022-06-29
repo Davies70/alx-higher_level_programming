96ec9ef5390b6f4613cad2715370c4acdf56eb1a
Ajayi <ajayidavies7@gmail.com>
2022-06-29 12:38:21 -0700

    finetuned the else ifs

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,13 +2,9 @@
 def uppercase(str):
     for letter in str:
         character = ord(letter)
-        if character >= 65 and character <= 95:
-            alpha = character
-        elif character >= 97 and character <= 122:
+        if character >= 97 and character <= 122:
             alpha = character - 32
-        elif character == 32:
-            alpha = 32
-        elif character >=48 and character <= 57:
+        else:
             alpha = character
         print("{:c}".format(alpha), end='')
     print("\n")
