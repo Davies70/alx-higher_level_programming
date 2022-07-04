21975fec596cc55af678102dd95751b222f70177
Ajayi <ajayidavies7@gmail.com>
2022-07-04 15:47:25 -0700

    1

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,8 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        length == None
-    first = sentence[0]
+        first == None
+    else:
+        first = sentence[0]
     data = (length, first)
     return data
