05addd388e27f8972056397bd1ba3ad3ff7e52f5
Ajayi <ajayidavies7@gmail.com>
2022-07-05 15:36:53 -0700

    final

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,25 @@
-#!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = (0, 0)
-        else:
-            tuple_a = (tuple_a[0], 0)
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = (0, 0)
-        else:
-            tuple_b = (tuple_b[0], 0)
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
