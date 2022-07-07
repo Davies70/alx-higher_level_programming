395ba4127b565620fd4878ad6b48234a2016078e
Ajayi <ajayidavies7@gmail.com>
2022-07-07 12:03:52 -0700

    1

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+def multiply_by_2(a_dictionary):
+    if a_dictionary == {}:
+        return a_dictionary
+    new_list = []
+    for key, value in a_dictionary.items():
+        new_list.append([key, value])
+    for x in new_list:
+        x[1] = x[1] * 2
+    jay = dict(new_list)
+    return jay
