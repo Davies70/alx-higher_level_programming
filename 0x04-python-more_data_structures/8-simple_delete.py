0d381770e3357e829896dd3ae99a3dfb6463eb99
Ajayi <ajayidavies7@gmail.com>
2022-07-07 10:46:16 -0700

    del keys

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,8 @@
 def simple_delete(a_dictionary, key=""):
     if key not in (a_dictionary.items()):
         return a_dictionary
+    elif a_dictionary == {}:
+        return a_dictionary
     else:
         del a_dictionary[key]
         return a_dictionary
