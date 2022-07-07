14afb8230923dda528b9d90d2f0bdc36c50e480a
Ajayi <ajayidavies7@gmail.com>
2022-07-07 06:59:14 -0700

    used enumerate

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    y = 0
+    if a_dictionary == {}:
+        return 0
+    for i, v in enumerate(a_dictionary):
+        num = i + 1
+    return num
