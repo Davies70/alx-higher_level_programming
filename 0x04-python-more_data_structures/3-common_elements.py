c60a64bbb4b6d277f6763777c9c397332ac282ce
Ajayi <ajayidavies7@gmail.com>
2022-07-07 06:18:18 -0700

    used add() to append in set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -4,6 +4,7 @@ def common_elements(set_1, set_2):
     for i in (set_1):
         if i in (set_2):
             result.add(i)
-            if len(result) == 0:
-                return set()
-    return result 
+    if len(result) == 0:
+        return set()
+    else:
+        return result 
