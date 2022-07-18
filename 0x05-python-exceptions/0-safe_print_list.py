260e03b544837fd4a0a6742c1514cfb8b6c6716a
Ajayi <ajayidavies7@gmail.com>
2022-07-18 10:52:32 -0700

    1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    if my_list == None:
+        return None
+    if x == 0:
+        return None
+    try:
+        while (i < x):
+            print(my_list[i], end='')
+            i += 1
+        print()
+        return i
+    except:
+        print()
+        return i
