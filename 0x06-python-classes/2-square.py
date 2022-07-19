de9d73a85e31a85ec04a666de6ee450219ee3cf4
Ajayi <ajayidavies7@gmail.com>
2022-07-19 16:45:43 -0700

    1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,11 +2,8 @@
 """ create a class Square """
 
 
-
 class Square:
-    """ initialize instance variable """
     def __init__(self, size=0):
-        """ instanstialize the variable size """
         self.__size = size
         if isinstance(self.__size, int) == False:
             raise TypeError("size must be an integer")
