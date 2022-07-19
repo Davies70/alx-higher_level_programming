094322ec20f24a2251a089025761f5608f8e162a
Ajayi <ajayidavies7@gmail.com>
2022-07-19 15:17:09 -0700

    2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" creating a Square class """
+
+
+class Square:
+    """ class created """
+
+    """ method instatation """
+
+
+    def __init__(self, size):
+        """ method created """
+        self.__size = size
