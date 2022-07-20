7259f72cd2e25b24c6cbb12a289178a3fc9b6465
Ajayi <ajayidavies7@gmail.com>
2022-07-20 12:28:48 -0700

    learned getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,16 +3,25 @@
 
 
 class Square:
+    """class created"""
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """ init method with arg size """
         self.__size = size
+
+    @property
     def size(self):
-        self.__size
+        return self.__size
+
+    @size.setter
     def size(self, value):
-        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
-        return self.__size**2
+        """public instance method"""
+        return self.__size ** 2
