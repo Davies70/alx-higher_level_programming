3481909ea3c6fe0a267ccf1cd22cee8197871882
Ajayi <ajayidavies7@gmail.com>
2022-07-19 17:22:09 -0700

    1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,10 @@ class Square:
     """ Square class created """
 
     def __init__(self, size=0):
-        """ square initialized with arg size, self """
+        """ square initialized with
+        Args:
+             size (int): size of square
+        """
         self.__size = size
         if isinstance(size, int) == False:
             raise TypeError("size must be an integer")
