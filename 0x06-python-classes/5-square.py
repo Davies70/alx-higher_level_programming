d527f3badc94517e09bb39ee137611acc28c8954
Ajayi <ajayidavies7@gmail.com>
2022-07-20 13:39:41 -0700

    1

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,29 +7,34 @@ class Square:
 
     def __init__(self, size=0):
         """ init method """
+
         self.__size = size
-    
+
     @property
     def size(self):
         """ getter method that returns size """
+
         return self.__size
 
     @size.setter
     def size(self, value):
         """ setter method """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         """ public instance method to return area of square """
+
         return self.__size ** 2
-    
+
     def my_print(self):
         """ public intance method that prints square """
+
         if self.__size == 0:
             print()
         else:
