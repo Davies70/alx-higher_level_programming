0f88b70485dc69590fd96f96d31816998da3c298
Ajayi <ajayidavies7@gmail.com>
2022-08-01 09:36:38 -0700

    comment

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" BaseGeomentary module """
+
+
+class BaseGeometry:
+    """ class declaration """
+    def area(self):
+        """ area method """
+        raise Exception("area() is not implemented")
