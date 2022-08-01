9a6043b129fea0ccac6c6da497521c5b749570c4
Ajayi <ajayidavies7@gmail.com>
2022-08-01 13:49:47 -0700

    comment

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the class BaseGemetry
+"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-""" Rectangle module """
-
-
 class Rectangle(BaseGeometry):
-    """ classs Rectangle inherits from class BG"""
+    """ The class empty"""
 
     def __init__(self, width, height):
-        """ attributes initiatlization """
-        self.__width = width
-        self.__height = height
+        """raises an exception when called"""
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.heigth = height
