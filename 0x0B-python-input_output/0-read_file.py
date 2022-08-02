1a1c306a6b21bac3bec0fab838e12ccb3c4c9452
Ajayi <ajayidavies7@gmail.com>
2022-08-02 14:41:08 -0700

    comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ function takes arg filename """
     with open(filename, 'r', encoding="utf-8") as file1:
         read_file = file1.read()
-    print(read_file)
+    print(read_file, end='')
