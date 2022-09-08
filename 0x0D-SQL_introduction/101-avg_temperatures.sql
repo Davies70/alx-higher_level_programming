98d24129b2e48a53e77e5d98bc9c074a24235b80
Ajayi <ajayidavies7@gmail.com>
2022-09-07 18:23:14 -0700

    without file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- temperatures
 
-SOURCE /alx-higher_level_programming/0x0D-SQL_introduction/temperatures.sql;
 SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
