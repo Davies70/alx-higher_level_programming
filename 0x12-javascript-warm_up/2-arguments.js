ebda33f83caa535d0144513d91e645a1b3b6eb0c
Ajayi <ajayidavies7@gmail.com>
2022-10-03 16:14:14 -0700

    arguments.argv.length

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* process arguments */
+
+const arg = process.argv;
+
+if (arg.length === 2)
+	console.log('No argument');
+if (arg.length === 3)
+	console.log('Argument found');
+if (arg.length > 3)
+	console.log('Arguments found');
