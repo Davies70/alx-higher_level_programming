c66128476085a992bf2dfcaa971c4ac77df948ab
Ajayi <ajayidavies7@gmail.com>
2022-10-04 09:04:50 -0700

    array.sort

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/* checks the biggest number */
+
+const arg = process.argv;
+const array = [];
+
+if (arg.length === 2 || arg.length === 3) {
+  console.log('1');
+} else {
+  for (let i = 2; i < arg.length; i++) {
+    array.push(arg[i]);
+  }
+  array.sort((a, b) => b - a);
+  console.log(array[1]);
+}
