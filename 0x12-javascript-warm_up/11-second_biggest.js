d2b75d59f25f91404999d009f6d4988366b158e7
Ajayi <ajayidavies7@gmail.com>
2022-10-04 09:10:23 -0700

    sort.length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ const arg = process.argv;
 const array = [];
 
 if (arg.length === 2 || arg.length === 3) {
-  console.log('1');
+  console.log(0);
 } else {
   for (let i = 2; i < arg.length; i++) {
     array.push(arg[i]);
