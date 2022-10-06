51fe73daf78deeb50b9198ea81753c4d613f034d
Ajayi <ajayidavies7@gmail.com>
2022-10-06 06:44:37 -0700

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,11 +3,11 @@
 exports.esrever = function (list) {
   let length = list.length - 1;
   const rArray = [];
-  let i = 0
+  let i = 0;
   while (i < list.length && length >= 0) {
     rArray[i] = list[length];
     i += 1;
     length -= 1;
   }
   return (rArray);
-}
+};
