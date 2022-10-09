557555733bd24f039047f4b3fa83ac33ed857024
Ajayi <ajayidavies7@gmail.com>
2022-10-09 07:23:58 -0700

    3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,8 @@
 starting with N (upper N) from the database hbtn_0e_0_usa
 Usage: script should take 3 arguments:
 mysql username, mysql password and database name
-(no argument validation needed)"""
+(no argument validation needed)
+"""
 import MySQLdb
 import sys
 
@@ -13,5 +14,5 @@ if __name__ == "__main__":
                               user=sys.argv[1], passwd=sys.argv[2],
                               db=sys.argv[3], charset="utf8")
     cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
     [print(rows) for rows in cur.fetchall()]
