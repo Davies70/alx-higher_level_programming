d4af8c29baeacdd01cad8df2cef69b9d524aacca
Ajayi <ajayidavies7@gmail.com>
2022-10-09 07:21:15 -0700

    3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
-"""
-
-Write a script that lists all states with a name
+"""Write a script that lists all states with a name
 starting with N (upper N) from the database hbtn_0e_0_usa
 Usage: script should take 3 arguments:
 mysql username, mysql password and database name
-(no argument validation needed)
-
-"""
-
-
+(no argument validation needed)"""
 import MySQLdb
 import sys
 
