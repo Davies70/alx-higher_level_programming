fa7022093027295cc11ac83a83b51a233ea7f22f
Ajayi <ajayidavies7@gmail.com>
2022-10-09 08:37:11 -0700

    1

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+filter states by user input
+script 4 arguments: mysql username, mysql password,
+database name and state name searched
+
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3])
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{:s}'
+                ORDER BY name ASC".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
