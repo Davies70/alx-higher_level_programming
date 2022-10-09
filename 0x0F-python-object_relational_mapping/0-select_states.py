d0d79abbb59b60fa0d08ed9d5ddb5ad1b4cfe9fd
Ajayi <ajayidavies7@gmail.com>
2022-10-09 06:01:38 -0700

    6

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name>
 
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
     connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3],
                               charset="utf8")
