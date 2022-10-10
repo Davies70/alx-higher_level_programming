2562c5cfcff4c1be13c906807ae86ac0f8deadf8
Ajayi <ajayidavies7@gmail.com>
2022-10-10 04:34:10 -0700

    0

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+An SQL search query safe from
+SQL injections
+
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3])
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
