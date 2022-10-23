9fb4e9bce871ce98d51b7ac84e20c46660b37c8a
Davies70 <ajayidavies7@gmail.com>
2022-10-23 13:50:04 -0700

    used regex

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL, 
-#and displays the size of the body of the response. The size must be displayed in bytes
-#You have to use curl
-
 curl -sI $1 | grep Content-Length | grep -oE '[0-9]*'
