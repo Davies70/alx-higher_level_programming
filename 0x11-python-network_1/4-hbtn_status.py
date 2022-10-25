0719af2e4c5eb45e9231633ddc00be005cfac6fa
Davies70 <ajayidavies7@gmail.com>
2022-10-25 04:17:03 -0700

    second try

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- {}'.format(type(r.text)))
-    print('\t- {}'.format(r.text))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
