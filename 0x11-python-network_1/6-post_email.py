1e24e078b4fc91fe85c9bbee98295f4b189c3aad
Davies70 <ajayidavies7@gmail.com>
2022-10-25 04:52:59 -0700

    post data

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,7 +6,8 @@ and finally displays the body of the response """
 import requests
 import sys
 
-dic = {}
-dic['email'] = sys.argv[2]
-r = requests.post(sys.argv[1], data=dic)
-print(r.text)
+if __name__ == '__main__':
+    dic = {}
+    dic['email'] = sys.argv[2]
+    r = requests.post(sys.argv[1], data=dic)
+    print(r.text)
