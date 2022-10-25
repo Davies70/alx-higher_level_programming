7eb34f15d9908a9e9a2108ef787f31669a6aca8f
Davies70 <ajayidavies7@gmail.com>
2022-10-25 02:45:24 -0700

    check2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 """
 Write a Python script that takes in a URL,sends a request to the URL and
 displays the value of the X-Request-Id variable
 """
-
 from urllib.request import urlopen
 import sys
 
-with urlopen(sys.argv[1]) as req:
-    dic = dict(req.headers)
-    print(dic.get('X-Request-Id'))
+if __name__ == '__main__':
+    with urlopen(sys.argv[1]) as req:
+        dic = dict(req.headers)
+        print(dic.get('X-Request-Id'))
