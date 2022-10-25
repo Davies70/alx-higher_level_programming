b98d7c79fd3e1076f09156d7bcc8d0f2b693d24a
Davies70 <ajayidavies7@gmail.com>
2022-10-25 04:47:14 -0700

    post data[H

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response """
+
+import requests
+import sys
+
+dic = {}
+dic['email'] = sys.argv[2]
+r = requests.post(sys.argv[1], data=dic)
+print(r.text)
