f06fd1546a1c58994c660610d6e9a897e8d80b18
Davies70 <ajayidavies7@gmail.com>
2022-10-25 10:00:39 -0700

    third

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,14 +11,9 @@ if __name__ == '__main__':
     r = sys.argv[1]
     o = sys.argv[2]
 
-    headers = {'accept': 'application/vnd.github+json', 'owner': o, 'repo': r}
     url = 'https://api.github.com/repos/' + o + '/' + r + '/commits'
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     api = response.json()
-    i = len(api) - 1
-    j = 10
-    while (i and j > 0):
+    for i in range(10):
         print('{}: {}'.format(api[i].get('sha'),
                               api[i].get('commit').get('author').get('name')))
-        i -= 1
-        j -= 1
