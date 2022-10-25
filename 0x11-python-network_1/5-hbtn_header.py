dd4579ac746a5f0afd054bff30d27f97b3d4db2e
Davies70 <ajayidavies7@gmail.com>
2022-10-25 04:37:24 -0700

    used get method

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,4 @@ import sys
 
 if __name__ == '__main__':
     r = requests.get(sys.argv[1])
-    if r.headers['X-Request-Id'] is not None:
-        print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
