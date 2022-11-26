8736fd9dfc3bbc8248b7c64c41d3f278b2b12904
Ajayi Davies <ajayidavies7@gmail.com>
2022-11-26 01:59:21 +0100

    final push

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,8 +1,5 @@
-#!/usr/bin/node
-
-$(function() {
-    const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
-    $.get(url, function(data, status){
-        $('#hello').text(data.hello);
+$('document').ready(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+      $('DIV#hello').text(data.hello);
     });
-});
+  });
